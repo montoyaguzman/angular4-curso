a5107b8840d1cb8e42df3525299271e7b8add08e
montoya <jose.montoya.contractor@beeva.com>
2017-10-05 09:45:42 -0500

    Funcional con prueba de Render

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -4,33 +4,41 @@ import { Component, Renderer2, ElementRef, ViewChild } from '@angular/core';
 @Component({  selector: 'app-formulario', templateUrl: './formulario.component.html' })
 
 export class FormularioComponent {
-  @ViewChild('myButton') myButton: ElementRef;
+  @ViewChild('op1') op1: ElementRef;
+  @ViewChild('op2') op2: ElementRef;
+  @ViewChild('op3') op3: ElementRef;
+  @ViewChild('op4') op4: ElementRef;
   public titulo_formulario = 'Formularios de Prueba';
   public roleVal: any;
   public check: boolean;
+  public bandera: boolean;
 
   constructor(private renderer: Renderer2) {
     this.roleVal = '';
     this.check = false;
+    this.bandera = false;
   }
 
-  disable() {
-    // this.myButton.nativeElement.setAttribute("disabled", "true"); //BAD PRACTICE
+  /*disable()
     this.renderer.setAttribute(this.myButton.nativeElement, 'disabled', 'true');
-  }
 
-  enable() {
-   // this.myButton.nativeElement.removeAttribute("disabled"); //BAD PRACTICE
-   this.renderer.removeAttribute(this.myButton.nativeElement, 'disabled');
-  }
+  enable()
+   this.renderer.removeAttribute(this.myButton.nativeElement, 'disabled');*/
 
   validar() {
-    if (this.check) {
-      this.disable();
+    if (!this.bandera) {
+      this.renderer.setAttribute(this.op2.nativeElement, 'checked', 'checked');
+      this.renderer.setAttribute(this.op3.nativeElement, 'disabled', 'true');
+      this.renderer.setAttribute(this.op4.nativeElement, 'disabled', 'true');
     }
-    if (!this.check) {
-      this.enable();
+    if (this.bandera) {
+      this.renderer.removeAttribute(this.op2.nativeElement, 'checked');
+      this.renderer.removeAttribute(this.op3.nativeElement, 'disabled');
+      this.renderer.removeAttribute(this.op4.nativeElement, 'disabled');
     }
+
+      this.bandera = !this.bandera;
+
   }
 
 }
