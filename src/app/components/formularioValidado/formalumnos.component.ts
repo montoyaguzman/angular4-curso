ae0afa2f3ccebdcd70530ff890189012b44cdfd4
montoya <montoyaguzman7@gmail.com>
2017-12-13 01:05:47 -0600

    valida controles parcialmente

--- src/app/components/formularioValidado/formalumnos.component.ts
+++ src/app/components/formularioValidado/formalumnos.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
-import {AbstractControl, FormControl, FormGroup, ValidatorFn, Validators,} from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { forbiddenNameValidator } from './forbiddenNama.directive';
 
 
-@Component({  selector: 'app-formalumnos', templateUrl: './formalumnos.component.html' })
+@Component({
+  selector: 'app-formalumnos',
+  templateUrl: './formalumnos.component.html'
+})
 
 export class FormalumnosComponent {
   public titulo_formalumnos = 'Formulario con validaciones';
-  public heroForm: any;
+  public alumnosForm: any;
   public nombre: string;
-  public alterego: string;
-  public poder: string;
 
+  ngOnInit(): void {
+    this.alumnosForm = new FormGroup({
+      'name': new FormControl(this.nombre, [
+        Validators.required,
+        Validators.minLength(4),
+        forbiddenNameValidator(/bobo/i) // <-- Here's how you pass in the custom validator.
+      ]),
+      // 'alterEgo': new FormControl(this.hero.alterEgo),
+      // 'power': new FormControl(this.hero.power, Validators.required)
+    });
+  }
+
+  registro() {
+
+  }
 
 
 }
